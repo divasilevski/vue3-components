86c4c01268375e653dc018af3daedda0aa6b217f
Dmitry Vasilevsky <dmitry.vasilevsky@immelman.ru>
2020-10-13 13:20:36 +0300

    simple card

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template lang="pug">
+img(alt="Vue logo" src="./assets/logo.png")
+.cards
+  SimpleCard(
+    image="http://orl.ec/images/%D0%9A%D0%BE%D1%82%D0%B8%D0%BA.jpg"
+    title="Some title"
+    copy="Some copy"
+  )
+  SimpleCard(
+    title="Some title Some subtitle Some subtitle"
+    copy="Some copy"
+  )
+  SimpleCard(
+    image="http://orl.ec/images/%D0%9A%D0%BE%D1%82%D0%B8%D0%BA.jpg"
+    copy="Some text Some subtitle Some subtitle Some subtitle Some subtitle Some subtitle"
+  )
+
+</template>
+
+<script>
+import SimpleCard from './components/Cards/SimpleCard.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SimpleCard,
+  },
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.cards {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-column-gap: 30px;
+  padding: 50px;
+}
+</style>
---
+++ src/components/Cards/SimpleCard.vue
@@ -0,0 +1,72 @@
+<template lang="pug">
+.card
+  .card__header
+    img(:src="image")
+
+  .card__body
+    h2.card__title(v-if="title") {{ title }}
+    .card__copy(v-if="copy") {{ copy }}
+</template>
+
+<script>
+export default {
+  props: {
+    image: {
+      type: String,
+      default:
+        'https://breakthrough.org/wp-content/uploads/2018/10/default-placeholder-image.png',
+    },
+    title: {
+      type: String,
+      default: '',
+    },
+    copy: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+// variables
+$bg: #eeeeee;
+$br: 4px;
+
+// classes
+.card {
+  position: relative;
+  background: $bg;
+  width: 100%;
+  border-radius: $br;
+
+  .card__header {
+    height: 300px;
+
+    img {
+      width: 100%;
+      height: 100%;
+      background-size: cover;
+      border-top-left-radius: $br;
+      border-top-right-radius: $br;
+    }
+  }
+
+  .card__body {
+    padding: 20px;
+
+    .card__title {
+      text-align: left;
+      margin: 0 0 15px 0;
+      font-weight: bold;
+      font-size: 20px;
+    }
+
+    .card__copy {
+      text-align: left;
+      margin: 0;
+      font-size: 14px;
+    }
+  }
+}
+</style>
